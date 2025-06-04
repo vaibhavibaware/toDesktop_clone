b70ba89d221ff6de3193117605969078368e302f
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-05 01:27:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3238F2',
+      },
+      fontFamily: {
+        display: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
